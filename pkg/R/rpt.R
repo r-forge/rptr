c7790ad9fdc2f2ca319814c5251da984bacf0ea1
schielz (Holger Schielzeth) <holger.schielzeth@uni-bielefeld.de>
2010-05-26 13:16:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rptr@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rpt.R
+++ pkg/R/rpt.R
@@ -8,25 +8,25 @@ rpt <- function(y, groups,
 			warning("Linear mixed model fitted by REML used by default. Change using argument 'method', if required ('corr', 'ANOVA', 'REML' and 'MCMC' allowed for Gaussian data).")
 			method<-"REML" 
 		}
-		if (method=="REML")  return(rpt.remlLMM(y, groups, CI, npermut, nboot))
-		if (method=="MCMC")  return(rpt.mcmcLMM(y, groups, CI))
-		if (method=="ANOVA") return(rpt.aov(y, groups, CI, npermut))	
-		if (method=="corr")  return(rpt.corr(y, groups, CI, npermut, nboot)) 
+		if (method=="REML")  return(rpt.remlLMM(y, groups, CI=CI, nboot=nboot, npermut=npermut))
+		if (method=="MCMC")  return(rpt.mcmcLMM(y, groups, CI=CI))
+		if (method=="ANOVA") return(rpt.aov(y, groups, CI=CI, npermut=npermut))	
+		if (method=="corr")  return(rpt.corr(y, groups, CI=CI, nboot=nboot, npermut=npermut)) 
 	}
 	if(datatype=="binomial" | datatype=="proportion") {
 		if(length(method)>1) {
 			warning("Generalised linear mixed model with multiplicative overdispersion fitted by PQL used by default. Change using argument 'method', if required ('GLMM.add' and 'GLMM.multi' allowed for Binomial data).")
 			method<-"GLMM.multi" 
 		}
-		if (method=="GLMM.multi") return(rpt.binomGLMM.multi(y, groups, link, CI, npermut, nboot))
-		if (method=="GLMM.add") return(rpt.binomGLMM.add(y, groups, CI))
+		if (method=="GLMM.multi") return(rpt.binomGLMM.multi(y, groups, link, CI=CI, nboot=nboot, npermut=npermut))
+		if (method=="GLMM.add") return(rpt.binomGLMM.add(y, groups, CI=CI))
 	}
 	if(datatype=="count") {
 		if(length(method)>1) {
 			warning("Generalised linear mixed model with multiplicative overdispersion fitted by PQL used by default. Change using argument 'method', if required ('GLMM.add' and 'GLMM.multi' allowed for count data).")
 			method<-"GLMM.multi"
 		}
-		if (method=="GLMM.multi") return(rpt.poisGLMM.multi(y, groups, link, CI, npermut, nboot)) 
-		if (method=="GLMM.add")   return(rpt.poisGLMM.add(y, groups, CI)) 
+		if (method=="GLMM.multi") return(rpt.poisGLMM.multi(y, groups, link, CI=CI, nboot=nboot, npermut=npermut)) 
+		if (method=="GLMM.add")   return(rpt.poisGLMM.add(y, groups, CI=CI)) 
 	} 
 }
