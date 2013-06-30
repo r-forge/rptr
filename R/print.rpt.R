d4b3e9fadc41f00dc2fad0cc3d02da8f1d316f2a
schielz (Holger Schielzeth) <holger.schielzeth@uni-bielefeld.de>
2013-06-30 08:26:09 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rptr@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/print.rpt.R
+++ R/print.rpt.R
@@ -12,11 +12,12 @@ print.rpt <- function(x, ...) {
 			"R  = ", round(x$R,3), "\n",
 			"SE = ", round(x$se,3), "\n",
 			"CI = [", round(x$CI.R[1],3), ", ", round(x$CI.R[2],3), "]", "\n",
-			"P  = ", signif(x$P[1], 3), " [", colnames(x$P)[1], "]", "\n", 
-			"     ", signif(x$P[2], 3), " [", colnames(x$P)[2], "]", "\n\n", 
+			"P  = ", signif(x$P[1], 3), " [", attr(x$P, "names")[1], "]", "\n", 
+			"     ", signif(x$P[2], 3), " [", attr(x$P, "names")[2], "]", "\n\n", 
 			sep="")
 	} 
 	if(x$datatype=="Gaussian" & length(x$P)==1 & length(x$R)>1) {
+		print("h3")
 		cat("\n", "Repeatability calculation using the ", x$method, " method", "\n\n")
 		for(i in 1: length(x$R)) {
 			cat("Repeatability for ", names(x$R)[i], "\n",
@@ -34,8 +35,8 @@ print.rpt <- function(x, ...) {
 			"R  = ", round(x$R[i],3), "\n",
 			"SE = ", round(x$se[i],3), "\n",
 			"CI = [", round(x$CI.R[i,1],3), ", ", round(x$CI.R[i,2],3), "]", "\n",
-			"P  = ", signif(x$P[i,1], 3), " [", colnames(x$P)[1], "]", "\n", 
-			"     ", signif(x$P[i,2], 3), " [", colnames(x$P)[2], "]", "\n\n", 
+			"P  = ", signif(x$P[i,1], 3), " [", attr(x$P, "names")[1], "]", "\n", 
+			"     ", signif(x$P[i,2], 3), " [", attr(x$P, "names")[2], "]", "\n\n", 
 			sep="")
 		}
 	}
